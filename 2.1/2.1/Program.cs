6fd19b00ac0db17d059acbbea504c7c38ca56aea
熙灏 张 <1262806950@qq.com>
2020-02-24 09:07:42 +0800

    1

---
+++ 2.1/2.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            num = int.Parse(Console.ReadLine());
+            int flag = (int)Math.Sqrt(num);
+            int factor = 2;
+            for (; factor <= flag; factor++)
+            {
+                while (num % factor == 0)
+                {
+                    Console.Write($"{factor},");
+                    num /= factor;
+                }
+            }
+            if (num >= factor)
+                Console.Write(factor);
+        }
+    }
+}
