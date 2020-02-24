61033b139b71ef8f9b3c5712071eba2a86abd322
熙灏 张 <1262806950@qq.com>
2020-02-24 09:51:06 +0800

    1

---
+++ 2.3/2.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[101];
+            for(int i=2; i < 101; i++)
+            {
+                array[i] = i;
+            }
+            for(int i = 2; i < 101; i++)
+            {
+                if (array[i] == 0)
+                    continue;
+                Console.Write($"{array[i]},");
+                for(int j = i + 1; j < 101; j++)
+                {
+                    if (array[j] % array[i] == 0)
+                        array[j] = 0;
+                }
+            }
+        }
+    }
+}
