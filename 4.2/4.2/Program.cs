15717200e8656baf5ee53f889fddd457e3366f31
熙灏 张 <1262806950@qq.com>
2020-03-09 09:46:33 +0800

    1

---
+++ 4.2/4.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _4._2
+{
+    public delegate void UseClock();
+
+    public class Clock
+    {
+        public event UseClock Tick;
+        public event UseClock Alarm;
+        public void Goon()
+        {
+            int times = 0 ;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Tick();
+                times += 1;
+                if (times % 10 == 0)
+                {
+                    times = 0;
+                    Alarm();
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Ticks()
+            {
+                Console.WriteLine(DateTime.Now.ToString());
+                Console.WriteLine("Tick");
+            }
+
+            void Alarms()
+            {
+                Console.WriteLine(DateTime.Now.ToString());
+                Console.WriteLine("Alarm");
+            }
+
+            Clock clock = new Clock();
+            clock.Tick += Ticks;
+            clock.Alarm += Alarms;
+            clock.Goon();
+        }
+    }
+}
